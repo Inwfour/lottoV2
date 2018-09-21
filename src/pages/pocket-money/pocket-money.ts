15a84460cdc06a0466d8b7d8eaafcb6c2f20882c
BosSaksit <saksit.kku@gmail.com>
2018-09-21 17:05:14 +0700

    add-history

--- src/pages/pocket-money/pocket-money.ts
+++ src/pages/pocket-money/pocket-money.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the PocketMoneyPage page.
@@ -14,12 +14,54 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'pocket-money.html',
 })
 export class PocketMoneyPage {
+  History:{
+    "Date":"1111111111",
+    "Amount":"20",
+    "Type":"Buy Coin"
+  };
+  History1 = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public Alert:AlertController) {
+    this.History1 = [
+    {
+      'Date': '22/08/18',        
+      'Amount': '20',
+      'Type': 'Sell Coin',
+      
+    },
+    {
+      'Date': '21/08/18',        
+      'Amount': '100',
+      'Type': 'Buy Coin',
+    },
+    {
+      'Date': '20/08/18',        
+      'Amount': '5',
+      'Type': 'Buy Ticket',
+    },
+    {
+      'Date': '19/08/18',        
+      'Amount': '200',
+      'Type': 'Scrath Poker',
+    },
+  ]
+
+    
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PocketMoneyPage');
+  }
+  show(item){
+    {
+      const alert = this.Alert.create({
+        title: item.Date + "<br>",
+        subTitle: item.Amount + "<br>" +item.Type,       
+        buttons: ['OK']
+      });
+      alert.present();
+    }
+
   }
 
 }
